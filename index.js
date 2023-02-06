48b5c4e88e4086f9b3e064c4a250835d59963783
shamira30 <shamila3011@googlemail.com>
2023-02-06 20:49:00 +0000

    added-variable-to-js

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 		showResults(questions, quizContainer, resultsContainer);
 	}
 }
+
+var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
