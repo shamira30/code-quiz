6d12bc0bc2fa5aeb8733081bd5c93e2bb4f0aaf6
shamira30 <shamila3011@googlemail.com>
2023-02-06 20:54:39 +0000

    added-generate-button-to-js

--- index.js
+++ index.js
@@ -116,3 +116,5 @@ submitButton.onclick = function(){
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
