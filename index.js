199562ae070e464bab7832e2307d3628b8dbac75
shamira30 <shamila3011@googlemail.com>
2023-02-06 21:09:05 +0000

    added-criteria-to-README

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 	function showQuestions(questions, quizContainer){
 		// code will go here
+        var myQuestions = [
+            {
+                question: "What is 10/2?",
+                answers: {
+                    a: '3',
+                    b: '5',
+                    c: '115'
+                },
+                correctAnswer: 'b'
+            },
+            {
+                question: "What is 30/3?",
+                answers: {
+                    a: '3',
+                    b: '5',
+                    c: '10'
+                },
+                correctAnswer: 'c'
+            }
+        ];
 	}
 
 	function showResults(questions, quizContainer, resultsContainer){
